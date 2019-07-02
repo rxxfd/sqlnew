d1e49656e51ab138a58b3b101f476107462ea818
ZhangMingShuo <34833093+ZhangMingShuo@users.noreply.github.com>
2019-07-03 00:13:19 +0800

    修改报名sql

--- participation.sql
+++ participation.sql
@@ -1,8 +1,8 @@
 create table participation(
 	aid int(11) NOT NULL ,
-	aname varchar(255) NOT NULL,
+	aname varchar(255),
 	isSign boolean ,
 	sid int(11) NOT NULL,
-	sname varchar(255) NOT NULL,
+	sname varchar(255),
 	PRIMARY KEY(aid,sid)
 )
